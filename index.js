5baf8bbf7465e53cee4c6b203bb8ff9c0e5fff3a
Heriberto <sosaheriberto2001@gmail.com>
2022-03-19 12:15:44 -0400

    2022-03-19T12:15:44-04:00

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const makeCommit = (n) => {
   const x = random.int(0, 54);
   const y = random.int(0, 6);
   const DATE = moment()
-    .subtract(2, "y")
+    .subtract(1, "y")
     .add(1, "d")
     .add(x, "w")
     .add(y, "d")
